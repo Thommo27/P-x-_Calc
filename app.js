b297bc533f606baf5c24c434ae894f654435542f
Thommo27 <joshua.thomas27@hotmail.com>
2019-06-09 01:03:53 +1000

    Added Function find factors of simple polynomial

--- app.js
+++ app.js
@@ -53,6 +53,12 @@ var dataController = (function() {
         }
     };
 
+    Term.prototype.isNeg = function() {
+        if (this.posOrNeg === '-') {
+            this.coe = -this.coe;
+        }
+    };
+
 
     return {
         createTerms: function() {
@@ -61,24 +67,36 @@ var dataController = (function() {
 
             // Creation of these terms could be moved to a private method
             // Leaving behinde the polynomial and csntTrmFactors 
+
             var firstTerm = new Term(getInput.firstOption, getInput.leadTerm, 3);
-            firstTerm.checkPower();
-            //console.log(firstTerm);
 
             var secondTerm = new Term(getInput.secondOption, getInput.secTerm, 2);
-            secondTerm.checkPower();
-            //console.log(secondTerm);
 
             var thirdTerm = new Term(getInput.thirdOption, getInput.thrdTerm, 1);
-            thirdTerm.checkPower();
-            //console.log(thirdTerm);
 
             var lastTerm = new Term(getInput.lastOption, getInput.lastTerm, 0);
-            lastTerm.checkPower();
+
+            protoInit();
 
             var polynomial = firstTerm.fullTerm + secondTerm.fullTerm + thirdTerm.fullTerm + lastTerm.fullTerm;
-            console.log(polynomial);
-            calculationsController.constantTermFactors(getInput.lastTerm);
+            console.log('P(x) =' + polynomial);
+
+            var gettingTerms = calculationsController.constantTermFactors(getInput.lastTerm);
+            gettingTerms(firstTerm.coe, secondTerm.coe, thirdTerm.coe, lastTerm.coe);
+
+            function protoInit() {
+                firstTerm.checkPower();
+                firstTerm.isNeg();
+
+                secondTerm.checkPower();
+                secondTerm.isNeg();
+
+                thirdTerm.checkPower();
+                thirdTerm.isNeg();
+
+                lastTerm.checkPower();
+                lastTerm.isNeg();
+            }
         }
     };
 
@@ -88,16 +106,31 @@ var dataController = (function() {
 var calculationsController = function() {
     
     return {
-        // Method needs to be adjusted to accomodate for -ve values
         constantTermFactors: function(k) {
+            var factList = [];
             for (var i = 0; i <= k; i++) {
                 if (k % i === 0) {
-                    console.log(i);
-                }
+                    factList.push(-i);
+                    factList.push(i);
+                }  
             }
-        }
-    };
+            console.log(factList);
 
+            return function(t1, t2, t3, t4) {
+                var list = factList;
+                for (var i = 0; i < list.length; i++) {
+                    var x = list[i];
+                    var testingTerms = (t1 * Math.pow(x, 3)) + (t2 * Math.pow(x, 2)) + (t3 * Math.pow(x, 1)) + parseInt(t4); // t4 a string before this point, need to fix
+                    if (testingTerms == 0) {
+                        console.log('P(' + x + ')' + ' = ' + t1 + '(' + x + ')3 ' + t2 + '(' + x + ')2 ' + t3 + '(' + x + ') ' + t4 + ' = 0');
+                        console.log('Therefore: (x' + x + ') is a factor');
+                    }
+                } 
+            };
+            
+        },
+        
+    };
 }();
 
 var controller = (function(UICtrl, dataCrtl, calcCtrl) {
@@ -105,16 +138,11 @@ var controller = (function(UICtrl, dataCrtl, calcCtrl) {
     var setupEventListeners = function() {
         var DOM = UICtrl.getDOMitems();
         
-        document.querySelector(DOM.subBtn).addEventListener('click', createTerms);
-        document.querySelector(DOM.subBtn).addEventListener('click', cnstTrmFactors);
-        
-        
+        document.querySelector(DOM.subBtn).addEventListener('click', createTerms);   
     };
 
-    var cnstTrmFactors  = calcCtrl.constantTermFactors;
     var createTerms = dataCrtl.createTerms;
     
-
     return {
         init: function() {
             setupEventListeners();
